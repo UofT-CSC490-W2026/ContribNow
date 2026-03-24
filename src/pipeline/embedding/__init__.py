cdd0b357725b3dddc93226219f57f0a37722734e
elidle <razan.ahsanrifandi@gmail.com>
2026-03-24 19:41:10 -0400

    Add basic embedding interfaces

---
+++ src/pipeline/embedding/__init__.py
@@ -0,0 +1,13 @@
+from src.pipeline.embedding.interfaces import (
+    EmbeddingConfig,
+    EmbeddingProvider,
+    EmbeddingRequest,
+    EmbeddingResult,
+)
+
+__all__ = [
+    "EmbeddingConfig",
+    "EmbeddingProvider",
+    "EmbeddingRequest",
+    "EmbeddingResult",
+]
